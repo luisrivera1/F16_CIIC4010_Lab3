26642076248368d05f4ed0789d13cce080977910
4015b18 <4015b18@platano.ece.uprm.edu>
2016-08-31 10:41:08 -0400

    Changed Name

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello Luis");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(850, 425);
+		myFrame.setLocation(850, 430);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
